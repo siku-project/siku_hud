86d0e97acb4270e56837a90a5e284a3edcdd4d45
Primordial-Devv <153821912+Primordial-Devv@users.noreply.github.com>
2026-08-30 22:37:15 +0200

    CHORE - Version 1.0.0 (#13)

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Siku Studio'
 description 'A modern, modular HUD for the SIKU ecosystem — delivering clean, responsive player vitals, navigation, voice, radio, and vehicle telemetry through a lightweight, customizable interface built for immersive FiveM roleplay experiences.'
-version '0.4.0'
+version '1.0.0'
 
 name 'siku_hud'
 
